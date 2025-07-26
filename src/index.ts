42c310f968f28a189242050c3ea4bf5e2fbe2eff
Sasha Koss <koss@nocorp.me>
2025-07-26 01:19:17 +0000

    Implement ty with clear error messages

--- src/index.ts
+++ src/index.ts
@@ -1,174 +1,188 @@
-import { $ } from "tdollar";
+import type { $ } from "tdollar";
 
-// @ts-expect-error -- WIP
-export const ty: Tyst = Object.assign(function tyst<Type>(
-  value: Type
-): Tyst.Signature<Type> {
-  // @ts-expect-error -- WIP
-  return {};
-},
-{});
+export const ty: Ty = function tyst<Type>(): Tyst<Type> {
+  return Object.assign(
+    (() => ({})) as unknown as Tyst.Signature<Type, "received">,
+    { is: () => {} }
+  );
+};
 
-interface Tyst {
-  <Type>(value: Type): Tyst.Signature<Type>;
+interface Ty {
+  <Type>(value: Type): Tyst<Type>;
 
-  <Type>(): Tyst.Signature<Type>;
+  <Type>(): Tyst<Type>;
+}
 
-  supertype: Tyst.Supertype;
-
-  exactly: Tyst.Exactly;
+export interface Tyst<
+  Type,
+  Position extends Tyst.Signature.Position = "expected"
+> extends Tyst.Signature<Type, Position> {
+  is: Tyst.Is<Type>;
 }
 
 namespace Tyst {
-  export interface Signature<Type> {
-    is: Is<Type>;
+  export interface Signature<Type, Position extends Signature.Position> {
+    (
+      phantom: SignaturePhantom,
+      // Allows to differentiate top types and proper types.
+      type: Signature.Type<Type>
+    ): // Allows to differentiate unions of different size,
+    // i.e. (`string | undefined` and `string`)
+    Signature.Type<Type>;
   }
 
-  export type Is<Supertype> = Expectation.Exactly<Supertype> & {
-    undefined: Expectation.Proper<Supertype, undefined>;
-  };
+  type SignaturePhantom = typeof signaturePhantom;
+  declare const signaturePhantom: unique symbol;
 
-  export interface Supertype {
-    <Type>(type: Type, callback: Supertype.Callback<Type>): void;
+  export namespace Signature {
+    export type Position = "expected" | "received";
 
-    <Type>(callback: Supertype.Callback<Type>): void;
+    export type Type<Type> = $.Is.Any<Type> extends true
+      ? Any
+      : $.Is.Never<Type> extends true
+      ? Never
+      : $.Is.Unknown<Type> extends true
+      ? Unknown
+      : Type;
+
+    type Any = $.Branded<"any", typeof any>;
+    declare const any: unique symbol;
+
+    type Never = $.Branded<"never", typeof never>;
+    declare const never: unique symbol;
+
+    type Unknown = $.Branded<"unknown", typeof unknown>;
+    declare const unknown: unique symbol;
   }
 
-  export namespace Supertype {
-    export interface Callback<Supertype> {
-      ($: $<Supertype>): void;
-    }
+  export type Is<Received> = Is.Exact<Received>;
 
-    export interface $<Supertype> {
-      of(signature: SignatureWIP<Supertype>): void;
-
-      exact<Type extends Supertype>(): SignatureWIP<Type>;
-    }
-  }
-
-  export interface Exactly {
-    <Type>(): SignatureWIP<Type>;
-  }
-
-  export interface SignatureWIP<Type> {
-    (value: Type): Type;
-  }
-
-  export namespace Expectation {
-    export type Proper<Received, Expected> = $.Is.Extreme<Received> extends true
-      ? never
-      : [Received] extends [Expected]
-      ? Fn
-      : never;
-
-    export interface Exactly<Expected> {
-      <Received>(
-        ...args: $.Or<$.Is.Any<Expected>, $.Is.Any<Received>> extends true
-          ? $.And<$.Is.Any<Expected>, $.Is.Any<Received>> extends true
-            ? []
-            : [Unexpected]
-          : $.Or<$.Is.Never<Expected>, $.Is.Never<Received>> extends true
-          ? $.And<$.Is.Never<Expected>, $.Is.Never<Received>> extends true
-            ? []
-            : [Unexpected]
-          : [Received, Expected] extends [Expected, Received]
-          ? []
-          : [Unexpected]
-      ): void;
-    }
-
-    type Unexpected = typeof unexpected;
-
-    declare const unexpected: unique symbol;
-
-    export interface Fn {
-      (): void;
+  export namespace Is {
+    export interface Exact<Received> {
+      (signature: Signature<Received, "received">): void;
     }
   }
 }
 
-// @tysts
+// @tysts: ty
 {
-  // tyst
+  // any
   {
-    // undefined
-    {
-      ty(undefined).is<undefined>();
-      ty({} as string | undefined).is<string | undefined>();
+    ty<any>().is(ty<any>());
 
-      // @ts-expect-error
-      ty({} as any).is<undefined>();
-      // @ts-expect-error
-      ty({} as unknown).is<undefined>();
-      // @ts-expect-error
-      ty({} as never).is<undefined>();
-      // @ts-expect-error
-      ty({} as string).is<string | undefined>();
-    }
+    // @ts-expect-error
+    ty<unknown>().is(ty<any>());
+    // @ts-expect-error
+    ty<never>().is(ty<any>());
+    // @ts-expect-error
+    ty<string>().is(ty<any>());
 
-    // any
-    {
-      ty({} as any).is<any>();
+    let variance = ty<any>();
+    variance = ty({} as any);
 
-      // @ts-expect-error
-      ty({} as unknown).is<any>();
-      // @ts-expect-error
-      ty({} as never).is<any>();
-      // @ts-expect-error
-      ty({} as string).is<any>();
-    }
-
-    // unknown
-    {
-      ty({} as unknown).is<unknown>();
-
-      // @ts-expect-error
-      ty({} as any).is<unknown>();
-      // @ts-expect-error
-      ty({} as never).is<unknown>();
-      // @ts-expect-error
-      ty({} as string).is<unknown>();
-    }
-
-    // never
-    {
-      ty({} as never).is<never>();
-
-      // @ts-expect-error
-      ty({} as any).is<never>();
-      // @ts-expect-error
-      ty({} as unknown).is<never>();
-      // @ts-expect-error
-      ty({} as string).is<never>();
-    }
-
-    // void
-    {
-      ty(void 0 as void).is<void>();
-      ty({} as string | void).is<string | void>();
-
-      // @ts-expect-error
-      ty({} as any).is<void>();
-      // @ts-expect-error
-      ty({} as unknown).is<void>();
-      // @ts-expect-error
-      ty({} as never).is<void>();
-      // @ts-expect-error
-      ty({} as string).is<string | void>();
-    }
+    // @ts-expect-error
+    variance = ty<unknown>();
+    // @ts-expect-error
+    variance = ty<never>();
+    // @ts-expect-error
+    variance = ty<string>();
   }
 
-  // tyst.undefined
+  // unknown
   {
-    ty(undefined).is.undefined();
+    ty<unknown>().is(ty<unknown>());
 
     // @ts-expect-error
-    ty({} as any).is.undefined();
+    ty<any>().is(ty<unknown>());
     // @ts-expect-error
-    ty({} as unknown).is.undefined();
+    ty<never>().is(ty<unknown>());
     // @ts-expect-error
-    ty({} as never).is.undefined();
+    ty<string>().is(ty<unknown>());
+
+    let variance = ty<unknown>();
+    variance = ty({} as unknown);
+
     // @ts-expect-error
-    ty({} as string | undefined).undefined();
+    variance = ty({} as any);
+    // @ts-expect-error
+    variance = ty<never>();
+    // @ts-expect-error
+    variance = ty<string>();
+  }
+
+  // never
+  {
+    ty<never>().is(ty<never>());
+
+    // @ts-expect-error
+    ty<any>().is(ty<never>());
+    // @ts-expect-error
+    ty<any>().is(ty<never>());
+    // @ts-expect-error
+    ty<any>().is(ty<never>());
+
+    let variance = ty<never>();
+    variance = ty({} as never);
+
+    // @ts-expect-error
+    variance = ty<any>();
+    // @ts-expect-error
+    variance = ty<unknown>();
+    // @ts-expect-error
+    variance = ty<string>();
+  }
+
+  // void
+  {
+    ty<void>().is(ty<void>());
+    ty<void | string>().is(ty<string | void>());
+
+    // @ts-expect-error
+    ty<any>().is(ty<void>());
+    // @ts-expect-error
+    ty<unknown>().is(ty<void>());
+    // @ts-expect-error
+    ty<never>().is(ty<void>());
+    // @ts-expect-error
+    ty<string>().is(ty<string | void>());
+
+    let variance = ty<void>();
+    variance = ty(void 0 as void);
+
+    // @ts-expect-error
+    variance = ty<any>();
+    // @ts-expect-error
+    variance = ty<unknown>();
+    // @ts-expect-error
+    variance = ty<never>();
+    // @ts-expect-error
+    variance = ty<string>();
+  }
+
+  // undefined
+  {
+    ty<undefined>().is(ty<undefined>());
+    ty<string | undefined>().is(ty<string | undefined>());
+
+    // @ts-expect-error
+    ty<any>().is(ty<undefined>());
+    // @ts-expect-error
+    ty<unknown>().is(ty<undefined>());
+    // @ts-expect-error
+    ty<never>().is(ty<undefined>());
+    // @ts-expect-error
+    ty<string>().is(ty<string | undefined>());
+
+    let variance = ty<undefined>();
+    variance = ty(undefined);
+
+    // @ts-expect-error
+    variance = ty<any>();
+    // @ts-expect-error
+    variance = ty<unknown>();
+    // @ts-expect-error
+    variance = ty<never>();
+    // @ts-expect-error
+    variance = ty<string>();
   }
 }
